d34241445772106126b248facb4ec1ba9150f094
James <jamesdesousa45@gmail.com>
2020-12-22 23:14:44 -0500

    commit tree 2e65cd3ca40f581b1ad87494cae38187b53f26ca

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE projects (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    funding INTEGER,
+    start_date DATE,
+    end_date DATE,
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  users_id INTEGER,
+  projects_id INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO projects VALUES ("coding", "education", 500, "11-05-2019", "12-01-2020");
+
+INSERT INTO users VALUES ("James", 22);
+
+INSERT INTO pledge VALUES (200, 1, 1);
